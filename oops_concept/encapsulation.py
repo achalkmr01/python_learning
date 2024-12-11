114119e68fb2eceaa487e0d76fbb56df88554275
achalsingh <achalkmr01@gmail.com>
2024-12-11 23:19:02 +0530

    new added

--- oops_concept/encapsulation.py
+++ oops_concept/encapsulation.py
@@ -15,12 +15,17 @@ class Car:
     def __init__(self, model):
         # This is an instance attribute
         self.model = model
+        for j in self.model:
+            print(f"New Car Model is {j}")
+        self.model = model
         for i in self.available_models:  # Loop through the list of models
             print(f"- {i}")  # Print each model
         print(f"Car model '{self.model}' has started.")
+        for i in self.available_models:
+            print(f"Car Model Name is {i}")
 
 
 # Creating an instance of the Car class
-car = Car("Ferrari")
+car = Car(["Ferrari","Hundai"])
 
 
